f6038b6e30bf0b8c37c2e9533c0d3d5755f50381
Charalampidis Teocharis <th.charalampidis@dei.gr>
2025-07-25 13:09:59 +0300

    Final Form; Two .py files (main,funcs);

---
+++ funcs.py
@@ -0,0 +1,64 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+
+def fill_Nans(df):
+    df = df.copy()
+    for col in df.columns:
+        if df[col].notna().any():
+            first_valid = df[col].dropna().iloc[0]
+            first_valid_label = df[col].first_valid_index()
+            first_valid_pos = df.index.get_loc(first_valid_label)
+            df[col] = df[col].fillna("ON" if first_valid == "ON" else "STOP")
+            df[col].iloc[:first_valid_pos] = (
+                "ON" if first_valid == "ON" else "STOP")
+    return df
+
+def line47Table(df):
+    df = df[df['production_line_id'] == 'gr-np-47']
+    line47Start = df.loc[df['status'] == "START", 
+                                ['timestamp']].rename(columns = {'timestamp': 'start_timestamp'}).sort_values(by='start_timestamp', ascending=True).reset_index(drop=True)
+    line47Stop = df.loc[df['status'] == "STOP", 
+                                ['timestamp']].rename(columns = {'timestamp' : 'stop_timestamp'}).sort_values(by='stop_timestamp', ascending=True).reset_index(drop=True)
+    line47Events = pd.concat([line47Start, line47Stop], axis=1)
+    line47Events['duration'] = pd.to_datetime(line47Events['stop_timestamp']) - pd.to_datetime(line47Events['start_timestamp'])
+    print("Line 47 Table:")
+    print(line47Events)
+    return(line47Events)
+
+def pivotData(df):
+    pivot = df.pivot(
+    index='timestamp', columns='production_line_id', values='status').reset_index()
+    pivot.columns.name = None
+    pivot = pivot.sort_index().ffill()
+    pivot = fill_Nans(pivot)
+    pivot['timedelta'] = pivot['timestamp'].diff()
+    return(pivot)
+
+def productionFloorStats(df):
+    pivot = pivotData(df)
+    onlineStatuses = ['ON', 'START']
+    pivot['atLeast1Line'] = pivot.apply(
+        lambda row: int(row.isin(onlineStatuses).any()), axis=1)
+    # print(pivot.to_string())
+    total_uptime = pivot['timedelta'][pivot['atLeast1Line'] == 1].sum()
+    print(f"Total Production Floor Uptime: {total_uptime}")
+    total_downtime = pivot['timedelta'][pivot['atLeast1Line'] == 0].sum()
+    print(f"Total Production Floor Downtime: {total_downtime}")
+    return(total_uptime,total_downtime)
+
+def highestDowntime(df):
+    downtime_list = []
+    df = pivotData(df)
+    for col in df.columns:
+        if col not in ['timestamp', 'timedelta', 'atLeast1Line']:
+            stop_rows = df[df[col] == 'STOP']
+            total_downtime = stop_rows['timedelta'].sum()
+            downtime_list.append((col, total_downtime))
+    downtime_df = pd.DataFrame(downtime_list, columns=['line', 'downtime'])
+
+    max_row = downtime_df.loc[downtime_df['downtime'].idxmax()]
+
+    print(f"Production line with most downtime: {max_row['line']}")
+    print(f"Total downtime: {max_row['downtime']}")
+
--- main.py
+++ main.py
@@ -1,93 +1,18 @@
-import mysql.connector
 import pandas as pd
-import numpy as np
+from funcs import line47Table,productionFloorStats,highestDowntime
+import warnings
+warnings.filterwarnings("ignore") #I know what this looks like; 
+#warning ignored:FutureWarning: ChainedAssignmentError: behaviour will change in pandas 3.0!
 
 
-def ensure_database(db_config):
-    temp_config = db_config.copy()
-    temp_config.pop('database')
-    conn = mysql.connector.connect(**temp_config)
-    cursor = conn.cursor()
-    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_config['database']}")
-    cursor.close()
-    conn.close()
+events_df = pd.read_csv('dataset.csv', parse_dates=['timestamp'])
 
-def load_data(file, db_config):
-    ensure_database(db_config)
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-    dataset = pd.read_csv(file, parse_dates=['timestamp'])
-    try:
-        cursor.execute("SELECT count(*) FROM events;")
-        table_len = cursor.fetchone()[0]
-    except Exception:
-        table_len = 0
-    if len(dataset) == 0:
-        print("The dataset is empty.")
-    elif dataset.isnull().values.any():
-        print("The dataset contains null values.")
-    elif table_len == len(dataset):
-        print("The dataset is already loaded in the database.")
-    else:
-        print("Loading data into the database...")
-        # Replace table using pandas to_sql with SQLAlchemy engine
-        dataset.to_sql('events', engine, if_exists='replace', index=False)
-    cursor.close()
-    conn.close()
+#Line 47 Table creation
+line47Table(events_df)
 
-def createL47(db_config):
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-    cursor.execute("create table if not exists Line47 "
-                  "(start_timestamp datetime,"
-                   "end_timestamp datetime,"
-                   "duration time)" )
-    
-def queryExecution(query,db_config):
-    conn = mysql.connector.connect(**db_config)
-    df = pd.read_sql(query,conn)
-    conn.close()
-    return df
-    
-def fill_Nans(df):
-    df = df.copy()
-    for col in df.columns:
-        if df[col].notna().any():
-            firstValid = df[col].dropna().iloc[0]
-            first_valid_label = df[col].first_valid_index()
-            first_valid_pos = df.index.get_loc(first_valid_label)
-            df[col] = df[col].fillna("ON" if firstValid == "ON" else "STOP")
-            df[col].iloc[:first_valid_pos] = ("ON" if firstValid == "ON" else "STOP")
-    return df
+# Production Floor Stats (Up/Downtime)
+productionFloorStats(events_df)
 
-db_config = {
-    'user': 'runtheorun',
-    'password': '9TDidJ0w',
-    'host': 'localhost',
-    'database': 'witside'
-}
+# Production Line with the most downtime
+highestDowntime(events_df)
 
-from sqlalchemy import create_engine
-engine = create_engine(
-f"mysql+mysqlconnector://{db_config['user']}:{db_config['password']}@{db_config['host']}/{db_config['database']}"
-)
-
-load_data('dataset.csv', db_config)
-
-#Production Floor Stats (Up/Downtime)
-
-events_df = queryExecution("select * from events order by production_line_id, timestamp",db_config)
-
-
-pivot = events_df.pivot(index='timestamp', columns='production_line_id', values='status').reset_index()
-pivot.columns.name = None
-pivot = pivot.sort_index().ffill()
-pivot = fill_Nans(pivot)
-pivot['timedelta'] = pivot['timestamp'].diff()
-onlineStatuses = ['ON','START']
-pivot['atLeast1Line'] = pivot.apply(lambda row: int(row.isin(onlineStatuses).any()),axis=1)
-print(pivot.to_string())
-total_uptime = pivot['timedelta'][pivot['atLeast1Line'] == 1].sum()
-print("Total Production Floor Uptime: "+total_uptime)
-total_downtime = pivot['timedelta'][pivot['atLeast1Line'] == 0].sum()
-print("Total Production Floor Downtime: "+total_downtime)
